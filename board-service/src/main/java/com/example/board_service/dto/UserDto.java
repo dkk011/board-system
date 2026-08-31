d6ed1ef20194fc1b372912ce056c351107445bbe
공덕규 <cleolclcyu@gongdeoggyuui-iMac.local>
2026-08-31 11:10:35 +0900

    first commit

---
+++ board-service/src/main/java/com/example/board_service/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.board_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+}
